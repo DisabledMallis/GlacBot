1b75b899a33f7e4aaed232a27b34bd2fffa5c4bc
ibx34 <Guamjust@gmail.com>
2021-10-31 01:38:23 -0400

    okay!

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod cache;
+mod commands;
+
+use std::env;
+use std::{collections::HashMap,
+          sync::{Arc, Mutex}};
+
+use anyhow::Result;
+use dotenv;
+
+fn main() {
+    println!("Hello, world!");
+}
